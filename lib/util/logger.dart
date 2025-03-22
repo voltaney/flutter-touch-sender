5ea56e36f4fa422dbcb8a510afca78bb1005e09a
voltaney <voltaney61@gmail.com>
2025-03-23 04:18:43 +0900

    refactor logger to use SimplePrinter for improved log formatting

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -1,7 +1,8 @@
 import 'package:logger/logger.dart';
+
 Logger get logger => Log.instance;
 
 class Log extends Logger {
-  Log._() : super(printer: PrettyPrinter(printTime: true));
+  Log._() : super(printer: SimplePrinter());
   static final instance = Log._();
 }
