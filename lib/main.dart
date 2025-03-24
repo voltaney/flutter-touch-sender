12a5949ce603be145ef38d9e96a49ad9668f4282
voltaney <voltaney61@gmail.com>
2025-03-24 12:15:48 +0900

    remove unused imports and UdpSender class from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,7 +7,6 @@ import 'package:touch_sender/l10n/app_localizations.dart';
 import 'package:touch_sender/provider/settings_provider.dart';
 import 'package:touch_sender/provider/shared_preferences_provider.dart';
 import 'package:touch_sender/router/app_router.dart';
-import 'package:touch_sender/util/logger.dart';
 
 final ThemeData lightTheme = ThemeData(
   useMaterial3: true,
@@ -34,54 +31,6 @@ Future<void> main() async {
       child: const MainApp(),
     ),
   );
-}
-
-class UdpSender {
-  final String _targetIp = '192.168.0.15';
-  Timer? sendTimer;
-  void startTimer() async {
-    if (sendTimer != null) {
-      if (sendTimer?.isActive ?? false) {
-        sendTimer?.cancel();
-        logger.d('Timer canceled');
-        return;
-      }
-    } else {
-      logger.d('Timer not found. Start new timer');
-    }
-    var count = 0;
-    var fails = 0;
-    final srcPort = 60000;
-    final targetPort = 60001;
-    const Duration interval = Duration(microseconds: 10);
-    // UDPソケットをバインド
-    RawDatagramSocket socket;
-    try {
-      socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, srcPort);
-    } on SocketException catch (e) {
-      logger.e('ソケットのバインドに失敗しました', error: e);
-      return;
-    }
-    sendTimer = Timer.periodic(interval, (timer) {
-      String message = 'tick(${timer.tick}) count($count)\n';
-      if (!socket.writeEventsEnabled) return;
-      // Uint8List data = Uint8List.fromList(message.codeUnits);
-
-      final dataSize = socket.send(
-        const Utf8Codec().encode(message),
-        InternetAddress(_targetIp),
-        targetPort,
-      );
-      if (dataSize <= 0) {
-        fails += 1;
-      } else {
-        count += 1;
-      }
-      // logger.d(
-      //   'tick(${timer.tick}) count($count) fails($fails) hertz(${count / (timer.tick * 10 / 1000)}) ($dataSize)',
-      // );
-    });
-  }
 }
 
 class MainApp extends ConsumerWidget {
